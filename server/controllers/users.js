5d5d36b4d3f1226e9d65a1299293bdc38ab1c4ba
Brook Kebede <brookabyss@gmail.com>
2017-07-11 20:30:44 -0700

    Login and reg works needs some work on displaying validation errros all at once

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -7,12 +7,16 @@ module.exports ={
       res.render('index')
     },
   register: function(req,res){
-    console.log("body",req.body)
     let first_name= req.body.first_name,last_name=req.body.last_name,password=req.body.password,c_password=req.body.confirm,dob= req.body.birthday,email=req.body.email
     // console.log(first_name,last_name,dob,password,email)
     let confirm_error={message:""}
     if (password!=c_password){
       confirm_error['message']= "Password confirmation doesn't match"
+      res.render('index', {title: 'you have errors!', errors: [confirm_error]})
+    }
+    else if(!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,32}/.test(password)){
+      console.log("password matcher error")
+      confirm_error['message']=  "Password failed validation, you must have at least 1 number, uppercase and special character"
       res.render('index', {title: 'you have errors!', errors: [confirm_error]})
     }
     password=bcrypt.hashSync(password, bcrypt.genSaltSync(8));
@@ -35,14 +39,11 @@ module.exports ={
       else {
           User.findOne({email:email},function(err,user){req.session.id=user.id})
           this.status=true
-          console.log("user status from controller",this.status)
-          console.log("session",req.session.id)
           res.render('show');
       }
     })
   },
   login:function(req,res){
-    // console.log(req.body)
     let password=req.body.password,email=req.body.email
     User.findOne({email:email},function(err,user){
       console.log("USERRRRRRRRR_______",user);
@@ -57,9 +58,7 @@ module.exports ={
         res.render('show')
         console.log('success')
       }
-      // else if ()){
-      //   res.render('index', {title: 'you have errors!', errors: user.errors})
-      // }
+      
 
     })
   },
